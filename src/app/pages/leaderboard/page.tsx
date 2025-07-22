0ae93e137f627900b55822f80c6d57c941c2c03f
Dennis van Leeuwen <bendora@gmail.com>
2025-07-22 13:27:42 +0200

    feat(layout): file structure, routing, pages

---
+++ src/app/pages/leaderboard/page.tsx
@@ -0,0 +1,4 @@
+// src/app/pages/loaderboard/page.tsx
+export default function LeaderboardPage() {
+  return <h1 className="text-2xl font-bold">Leaderboard Page</h1>;
+}
---
+++ src/app/pages/roster/page.tsx
@@ -0,0 +1,4 @@
+// src/app/pages/roster/page.tsx
+export default function RosterPage() {
+  return <h1 className="text-2xl font-bold">Roster Page</h1>;
+}
